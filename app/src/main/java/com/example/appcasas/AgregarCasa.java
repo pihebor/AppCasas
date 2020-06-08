83ff31c7af47b16579b4b392ccdce1fc3ec6fb26
Pedro Hernandez <pihebor@hotmail.com>
2020-06-07 19:14:45 -0500

    Se agrega funcionalidad a la Clase Agregar Casas, con una foto aleatoria y ocultando el teclado para que se vea el snakbar

--- app/src/main/java/com/example/appcasas/AgregarCasa.java
+++ app/src/main/java/com/example/appcasas/AgregarCasa.java
@@ -2,13 +2,77 @@ package com.example.appcasas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class AgregarCasa extends AppCompatActivity {
+    private ArrayList<Integer> fotos;
+    private EditText escritura, departamento, ciudad, barrio, direccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_casa);
+
+        escritura = findViewById(R.id.txtEscritura);
+        departamento = findViewById(R.id.txtDepartamento);
+        ciudad = findViewById(R.id.txtCiudad);
+        barrio = findViewById(R.id.txtBarrio);
+        direccion = findViewById(R.id.txtDireccion);
+
+        fotos = new ArrayList<>();
+        fotos.add(R.drawable.casa1);
+        fotos.add(R.drawable.casa2);
+        fotos.add(R.drawable.casa3);
+        fotos.add(R.drawable.casa4);
+        fotos.add(R.drawable.casa5);
+
+    }
+
+    public void guardar(View v){
+        String escri, dep, ciu, bar, dir;
+        int fot;
+        Casa casa;
+        InputMethodManager imp = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        escri = escritura.getText().toString();
+        dep = departamento.getText().toString();
+        ciu = ciudad.getText().toString();
+        bar = barrio.getText().toString();
+        dir = direccion.getText().toString();
+        fot = fotoAleatoria();
+        casa = new Casa(escri, dep, ciu, bar, dir, fot);
+        casa.guardar();
+        limpiar();
+        imp.hideSoftInputFromWindow(escritura.getWindowToken(), 0);
+        Snackbar.make(v, getString(R.string.mensaje_guardar),Snackbar.LENGTH_LONG).show();
+    }
+
+    public void limpiar(View v){
+        limpiar();
+    }
+
+    public int fotoAleatoria(){
+        int foto_seleccionada;
+        Random r = new Random();
+        foto_seleccionada = r.nextInt(this.fotos.size());
+        return fotos.get(foto_seleccionada);
+    }
+
+    public void limpiar(){
+        escritura.setText("");
+        departamento.setText("");
+        ciudad.setText("");
+        barrio.setText("");
+        direccion.setText("");
+        escritura.requestFocus();
     }
 }
--- app/src/main/java/com/example/appcasas/MainActivity.java
+++ app/src/main/java/com/example/appcasas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appcasas;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,34 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.btnAgregar);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void agregar(View v){
+        Intent i;
+        i = new Intent(MainActivity.this, AgregarCasa.class);
+        startActivity(i);
+        finish();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
